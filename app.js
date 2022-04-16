98c1a56a638a69301d78bd5cba483dcb5b90942c
codename-24 <102291062+codename-24@users.noreply.github.com>
2022-04-16 17:06:21 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const loginRoute = require('./routes/login');
+const messageRoute = require('./routes/message');
+
+app.use(bodyParser.urlencoded({extended:false})
+
+app.use(loginRoute);
+app.use(messageRoute);
+app.listen(4500);
